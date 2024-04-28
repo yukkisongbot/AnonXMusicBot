c1fd227e1bffc32624d6f52f78abde2f2e193f1f
yukkisongbot <168181662+yukkisongbot@users.noreply.github.com>
2024-04-28 17:33:53 +0530

    Update play.py
    Signed-off-by: yukkisongbot <168181662+yukkisongbot@users.noreply.github.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -66,6 +66,10 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    ]
+    return buttons
+
 
 def stream_markup(_, chat_id):
     buttons = [
@@ -76,6 +80,9 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+    ]
+    return buttons
 
 
 def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
