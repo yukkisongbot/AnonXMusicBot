eea42c24818d2c55c7dfdd9fcf9699120598392c
yukkisongbot <168181662+yukkisongbot@users.noreply.github.com>
2024-04-28 17:29:22 +0530

    Update play.py
    Signed-off-by: yukkisongbot <168181662+yukkisongbot@users.noreply.github.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "■—————————"
+        bar = "◉—————————"
     elif 10 < umm < 20:
-        bar = "—•————————"
+        bar = "—◉————————"
     elif 20 <= umm < 30:
-        bar = "——°———————"
+        bar = "——◉———————"
     elif 30 <= umm < 40:
-        bar = "———☆——————"
+        bar = "———◉——————"
     elif 40 <= umm < 50:
-        bar = "————¤—————"
+        bar = "————◉—————"
     elif 50 <= umm < 60:
-        bar = "—————¿————"
+        bar = "—————◉————"
     elif 60 <= umm < 70:
-        bar = "——————♧———"
+        bar = "——————◉———"
     elif 70 <= umm < 80:
-        bar = "———————◇——"
+        bar = "———————◉——"
     elif 80 <= umm < 95:
-        bar = "————————♤—"
+        bar = "————————◉—"
     else:
-        bar = "—————————♡"
+        bar = "—————————◉"
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
@@ -66,23 +66,16 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        [InlineKeyboardButton(text=_[" update "], url=" https://t.me/YukkiSongUpdate ")],
-    ]
-    return buttons
-
 
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="⛔️", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="⏸", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="¤", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="⛷", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="🛑", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [InlineKeyboardButton(text=_[" update "], url=" https://t.me/YukkiSongUpdate ")],
-    ]
-    return buttons
 
 
 def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
