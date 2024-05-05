02ef92ad9aa8c79bfd11267c4206c1e007fd16e0
yukkisongbot <168181662+yukkisongbot@users.noreply.github.com>
2024-05-05 19:47:46 +0530

    Update autobrodecast.py
    Signed-off-by: yukkisongbot <168181662+yukkisongbot@users.noreply.github.com>

--- AnonXMusic/plugins/sudo/autobrodecast.py
+++ AnonXMusic/plugins/sudo/autobrodecast.py
@@ -5,53 +5,56 @@ from AnonXMusic.utils.database import get_served_chats
 from config import LOGGER_ID
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+AUTO_GCASTS = True
 
-AUTO_GCAST = "True"
+START_IMG_URLS = "https://graph.org/file/58e9ada3e0cbae2268306.jpg"
 
-MESSAGES = f"""‣  тнιѕ ιѕ {app.mention}
- 
- ➜ α мυѕιᴄ ρℓαуєʀ вσт ωιтн ѕσмє α∂ναиᴄє∂ fєαтυʀєѕ."""
+MESSAGES = f"""**➜ α мυѕιᴄ ρℓαуєʀ вσт ωιтн ѕσмє α∂ναиᴄє∂ fєαтυʀєѕ
+
+➻ ᴅɪsᴄᴏᴠᴇʀ ᴀ ᴡᴏʀʟᴅ ᴏғ ᴇɴᴅʟᴇss ᴍᴜsɪᴄ ᴘᴏssɪʙɪʟɪᴛɪᴇs ᴡɪᴛʜ ˹Yukki ꭙ ᴍᴜsɪᴄ˼ ᴛʜᴇ ᴜʟᴛɪᴍᴀᴛᴇ ᴛᴇʟᴇɢʀᴀᴍ ᴍᴜsɪᴄ ʙᴏᴛ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ғᴇᴀᴛᴜʀᴇs.
+
+• ᴘʀᴏᴍᴏᴛɪᴏɴ / ᴀᴅs ғʀᴇᴇ ʙᴏᴛ.
+• 24 ʜʀ ᴜᴘᴛɪᴍᴇ.
+• ʟᴀɢ ғʀᴇᴇ sᴍᴏᴏᴛʜ ᴍᴜsɪᴄ ᴇxᴘᴇʀɪᴇɴᴄᴇ.
+
+ᴀᴅᴅ @Yukkisongbot ɴᴏᴡ ᴀɴᴅ ʟᴇᴛ ᴛʜᴇ ᴍᴜsɪᴄ ᴛᴀᴋᴇ ᴏᴠᴇʀ ʏᴏᴜʀ ᴡᴏʀʟᴅ!**"""
 
 
 BUTTONS = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(
-                "α∂∂ ʏᴜᴋᴋɪ ιи уσυʀ ɢʀσυρ",
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
+            InlineKeyboardButton("𝙰𝚍𝚍 𝙼𝚎", url=f"https://t.me/YukkiSongBot?startgroup=true")
         ]
     ]
 )
 
-caption = MESSAGES
 
+
+TEXT = """ᴀᴜᴛᴏ ɢᴄᴀsᴛ ɪs ᴇɴᴀʙʟᴇᴅ sᴏ ᴀᴜᴛᴏ ɢᴄᴀsᴛ/ʙʀᴏᴀᴅᴄᴀsᴛ ɪs ᴅᴏɪɴ ɪɴ ᴀʟʟ ᴄʜᴀᴛs ᴄᴏɴᴛɪɴᴜᴏᴜsʟʏ. \nɪᴛ ᴄᴀɴ ʙᴇ sᴛᴏᴘᴘᴇᴅ ʙʏ ᴘᴜᴛ ᴠᴀʀɪᴀʙʟᴇ [ᴀᴜᴛᴏ_ɢᴄᴀsᴛ = (ᴋᴇᴇᴘ ʙʟᴀɴᴋ & ᴅᴏɴᴛ ᴡʀɪᴛᴇ ᴀɴʏᴛʜɪɴɢ)]"""
 
 async def send_text_once():
     try:
-        await app.send_message(LOGGER_ID, caption)
+        await app.send_message(LOGGER_ID, TEXT)
     except Exception as e:
         pass
-
 
 async def send_message_to_chats():
     try:
         chats = await get_served_chats()
 
         for chat_info in chats:
-            chat_id = chat_info.get("chat_id")
-            if isinstance(chat_id, int):
+            chat_id = chat_info.get('chat_id')
+            if isinstance(chat_id, int):  # Check if chat_id is an integer
                 try:
-                    await app.send_message(chat_id, photo=caption, reply_markup=BUTTONS)
-                    await asyncio.sleep(10)
+                    await app.send_photo(chat_id, photo=START_IMG_URLS, caption=MESSAGES, reply_markup=BUTTONS)
+                    await asyncio.sleep(20)  # Sleep for 100 second between sending messages
                 except Exception as e:
-                    pass
+                    pass  # Do nothing if an error occurs while sending message
     except Exception as e:
-        pass
-
+        pass  # Do nothing if an error occurs while fetching served chats
 
 async def continuous_broadcast():
-    await send_text_once()
+    await send_text_once()  # Send TEXT once when bot starts
 
     while True:
         if AUTO_GCAST:
@@ -59,8 +62,10 @@ async def continuous_broadcast():
                 await send_message_to_chats()
             except Exception as e:
                 pass
-        await asyncio.sleep(43200)
 
+        # Wait for 100000 seconds before next broadcast
+        await asyncio.sleep(100000)
 
-if AUTO_GCAST:
+# Start the continuous broadcast loop if AUTO_GCAST is True
+if AUTO_GCAST:  
     asyncio.create_task(continuous_broadcast())
