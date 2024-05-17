d4fcf0133fb1007c9f2126b6bbbad14cfeb55ace
yukkisongbot <168181662+yukkisongbot@users.noreply.github.com>
2024-05-17 10:31:46 +0530

    Update play.py
    Signed-off-by: yukkisongbot <168181662+yukkisongbot@users.noreply.github.com>

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -33,11 +33,11 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "██░░░░░░░░░░░░░░░░░░"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "░░░░██░░░░░░░░░░░░░"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "░░░░░░░░░██░░░░░░░░"
     elif 30 <= umm < 40:
         bar = "———◉——————"
     elif 40 <= umm < 50:
