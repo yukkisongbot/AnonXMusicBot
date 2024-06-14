80adc2cec74b54793d15d1c120e6ed70c2830f31
yukkisongbot <168181662+yukkisongbot@users.noreply.github.com>
2024-06-14 19:40:20 +0530

    Update start.py
    Signed-off-by: yukkisongbot <168181662+yukkisongbot@users.noreply.github.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -28,7 +28,6 @@ def private_panel(_):
  
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
         ],
     ]
     return buttons
